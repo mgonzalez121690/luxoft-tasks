2e774fd67da2bb4161b35ad601b533070481ea7d
Moises Gonzalez <moises.gonzaleza@gmail.com>
2021-03-04 16:43:19 -0600

    Changes for challenge

--- apollo-server-api/server.js
+++ apollo-server-api/server.js
@@ -10,36 +10,17 @@ const typeDefs = gql`
 
   type Query {
     users: [User]
-    user(name: String!): User!
   }
 `;
 
-// My blog: https://www.ibrahima-ndaw.com/
 
 const resolvers = {
   Query: {
     users: async () => {
       try {
         const users = await axios.get("https://api.github.com/users");
-        return users.data.map(({ id, login, avatar_url }) => ({
-          id,
-          login,
-          avatar_url
-        }));
-      } catch (error) {
-        throw error;
-      }
-    },
-    user: async (_, args) => {
-      try {
-        const user = await axios.get(
-          `https://api.github.com/users/${args.name}`
-        );
-        return {
-          id: user.data.id,
-          login: user.data.login,
-          avatar_url: user.data.avatar_url
-        };
+        return users;
+
       } catch (error) {
         throw error;
       }
